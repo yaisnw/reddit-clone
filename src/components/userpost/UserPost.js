31f4b46d57de6713adf3dcf5a6c24391b1165e66
unknown <eliteyasin1@gmail.com>
2026-02-14 13:05:56 +0500

    fixed conditional rendering issues on UserPost.js

--- src/components/userpost/UserPost.js
+++ src/components/userpost/UserPost.js
@@ -1,33 +1,33 @@
-import React from 'react'
 import "./UserPost.css"
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import avatar from "./avatar.png"
 import Expanded from './Expanded';
 
 function UserPost({ post, key, onToggleExpansion }) {
-    const { subreddit_name_prefixed, thumbnail, selftext, author, title, ups,  } = post.data;
+    const { subreddit_name_prefixed, thumbnail, selftext, author, title, ups, } = post.data;
     const profilePictureObject = post?.data?.author_flair_richtext?.find(item => item.e === 'emoji');
     const profilePicture = profilePictureObject ? profilePictureObject.u : null;
     const video = post.data?.media?.reddit_video?.fallback_url
     const postId = post.data.id
-    const expanded = useSelector(state=> state.userposts.expandedPosts[postId])
+    const expanded = useSelector(state => state.userposts.expandedPosts[postId])
 
 
     return (
         <div className={expanded ? "userPostExpanded" : "userPost"} onClick={() => onToggleExpansion(postId)} >
             <section className='sub2'>
                 <div className='sub'>
-                    {profilePicture ? <img className='profile' src={profilePicture} /> : <img className='profile' src={avatar} /> }
+                    {profilePicture ? <img className='profile' src={profilePicture} alt="profile avatar" /> : <img className='profile' src={avatar} alt="default avatar" />}
                     <p>{subreddit_name_prefixed}</p>
                 </div>
                 <p>Posted by {author}</p>
             </section>
-            {selftext.length > 0 ? <p className='caption'>{selftext.length  >= 100 ? selftext.slice(0, 60) + "..." : selftext }</p> :  <p className='caption'>{title.length  >= 100 ? title.slice(0, 60) + "..." : title }</p>}
-            {thumbnail && <img src={thumbnail} alt="" />}
-            {video && !expanded && <video width="300px" height="200px" controls><source src={video} type="video/mp4"/></video>}
-            {expanded && <video width="500px" height="300px" controls><source src={video} type="video/mp4"/></video>}
+            {selftext.length > 0 ? <p className='caption'>{selftext.length >= 100 ? selftext.slice(0, 60) + "..." : selftext}</p> : <p className='caption'>{title.length >= 100 ? title.slice(0, 60) + "..." : title}</p>}
+            {!expanded && thumbnail && <img src={thumbnail} alt="" />}
+            {video && !expanded && <video width="300px" height="200px" controls><source src={video} type="video/mp4" /></video>}
+            {expanded && video && <video width="500px" height="300px" controls><source src={video} type="video/mp4" /></video>}
+            {expanded && !video && thumbnail && <img src={thumbnail} alt="" style={{ width: "100%", height: "auto" }} />}
             {expanded && (
-                <Expanded ups={ups}/>
+                <Expanded ups={ups} />
             )}
 
         </div>
