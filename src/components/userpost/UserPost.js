8570700cd13bea4dd8070cc3f694689f3ab153bc
unknown <eliteyasin1@gmail.com>
2026-02-14 14:40:03 +0500

    separated thumbnail and main image

--- src/components/userpost/UserPost.js
+++ src/components/userpost/UserPost.js
@@ -4,28 +4,56 @@ import avatar from "./avatar.png"
 import Expanded from './Expanded';
 
 function UserPost({ post, key, onToggleExpansion }) {
-    const { subreddit_name_prefixed, thumbnail, selftext, author, title, ups, } = post.data;
+    const { subreddit_name_prefixed, thumbnail, selftext, author, title, ups, url_overridden_by_dest } = post.data;
     const profilePictureObject = post?.data?.author_flair_richtext?.find(item => item.e === 'emoji');
     const profilePicture = profilePictureObject ? profilePictureObject.u : null;
-    const video = post.data?.media?.reddit_video?.fallback_url
-    const postId = post.data.id
-    const expanded = useSelector(state => state.userposts.expandedPosts[postId])
-
+    const video = post.data?.media?.reddit_video?.fallback_url;
+    const postId = post.data.id;
+    const expanded = useSelector(state => state.userposts.expandedPosts[postId]);
+    const isImage = url_overridden_by_dest?.match(/\.(jpg|jpeg|png|gif|webp)$/i);
 
     return (
-        <div className={expanded ? "userPostExpanded" : "userPost"} onClick={() => onToggleExpansion(postId)} >
+        <div className={expanded ? "userPostExpanded" : "userPost"} onClick={() => onToggleExpansion(postId)}>
             <section className='sub2'>
                 <div className='sub'>
-                    {profilePicture ? <img className='profile' src={profilePicture} alt="profile avatar" /> : <img className='profile' src={avatar} alt="default avatar" />}
+                    {profilePicture
+                        ? <img className='profile' src={profilePicture} alt="profile avatar" />
+                        : <img className='profile' src={avatar} alt="default avatar" />
+                    }
                     <p>{subreddit_name_prefixed}</p>
                 </div>
                 <p>Posted by {author}</p>
             </section>
-            {selftext.length > 0 ? <p className='caption'>{selftext.length >= 100 ? selftext.slice(0, 60) + "..." : selftext}</p> : <p className='caption'>{title.length >= 100 ? title.slice(0, 60) + "..." : title}</p>}
-            {!expanded && !video && thumbnail && <img src={thumbnail} alt="" />}
-            {!expanded && video && <video width="300px" height="200px" controls><source src={video} type="video/mp4" /></video>}
-            {expanded && video && <video width="500px" height="300px" controls><source src={video} type="video/mp4" /></video>}
-            {expanded && !video && thumbnail && <img src={thumbnail} alt="" style={{ width: "300px", height: "200px", objectFit: "contain" }} />}
+
+            {selftext.length > 0
+                ? <p className='caption'>{selftext.length >= 100 ? selftext.slice(0, 60) + "..." : selftext}</p>
+                : <p className='caption'>{title.length >= 100 ? title.slice(0, 60) + "..." : title}</p>
+            }
+
+            {!expanded && video && (
+                <video width="300px" height="200px" controls>
+                    <source src={video} type="video/mp4" />
+                </video>
+            )}
+            {!expanded && !video && isImage && (
+                <img src={url_overridden_by_dest} alt="" style={{ width: "100%", height: "auto" }} />
+            )}
+            {!expanded && !video && !isImage && thumbnail && (
+                <img src={thumbnail} alt="" />
+            )}
+
+            {expanded && video && (
+                <video width="500px" height="300px" controls>
+                    <source src={video} type="video/mp4" />
+                </video>
+            )}
+            {expanded && !video && isImage && (
+                <img src={url_overridden_by_dest} alt="" style={{ width: "300px", height: "200px", objectFit: "contain" }} />
+            )}
+            {expanded && !video && !isImage && thumbnail && (
+                <img src={thumbnail} alt="" style={{ width: "300px", height: "200px", objectFit: "contain" }} />
+            )}
+
             {expanded && ups > 0 && <Expanded ups={ups} />}
         </div>
     )
